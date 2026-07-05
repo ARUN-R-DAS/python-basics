ae2dc60c85c47a3dd9349d96abb04626f918660e
arun <arunrdas2@gmail.com>
2026-07-05 11:17:36 +0530

    draw square done

--- ResizeABox/main.py
+++ ResizeABox/main.py
@@ -12,14 +12,32 @@ clock = pygame.time.Clock()
 
 running = True
 
-while running:
+square_x = 100
+square_y = 100
+square_x_size = 50
+square_y_size = 50
+#----------------------------------------------
+def draw_square():
+    pygame.draw.rect(
+        screen,
+        'red',
+        (square_x, square_y, square_x_size, square_y_size)
+    )
+#----------------------------------------------
+def event_handling():
+    global running
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+#----------------------------------------------
+while running:
     
+    event_handling()
     screen.fill('black')
+
+    draw_square()
+
     pygame.display.flip()
     clock.tick(60)
-
 
 pygame.quit()
