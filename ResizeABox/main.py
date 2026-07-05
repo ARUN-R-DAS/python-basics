bd4cca5516695d177cc07aa5e703670005ae8f8f
arun <arunrdas2@gmail.com>
2026-07-05 11:12:35 +0530

    pygame basic structure

---
+++ ResizeABox/main.py
@@ -0,0 +1,25 @@
+import pygame
+
+pygame.init()
+
+WIDTH = 800
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("ResizeABox")
+
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill('black')
+    pygame.display.flip()
+    clock.tick(60)
+
+
+pygame.quit()
