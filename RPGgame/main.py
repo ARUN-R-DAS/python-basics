19ec5525a5f1a61b4e8e2b4e86fcf728d738bf94
arun <arunrdas2@gmail.com>
2026-07-07 19:36:37 +0530

    fixes ?

--- RPGgame/main.py
+++ RPGgame/main.py
@@ -1,18 +1,24 @@
-from .player import Player
+from player import Player
 
 player = Player()
 
 while True:
+    print("--------------------------------------------")
     print("Commands Available:")
     print("1: take 10 damage")
     print("2: heal 5 health")
-    try:
-        input = int("Input: ")
-    except:
-        print("Invalid input")
     
-    match input:
-        case 1:
-            player.take_damage(10)
-        case 2:
-            player.heal(10)
+    try:
+        i = int(input("input: "))
+    except:
+        print(">>>>>>>>>>>>>>>Invalid Input")
+        continue
+    print("--------------------------------------------")
+    if i == 1:
+        player.take_damage(10)
+        print("Player took 10 damage")
+    elif i == 2:
+        player.heal(5)
+        print("Player healed 5 health")
+
+    print(f"Player Health: {player.health}")
